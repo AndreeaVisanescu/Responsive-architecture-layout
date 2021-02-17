3bcdede0ca0ca82ba681a2b8f080e9fd84a591f0
Visanescu Andreea <visanescu.andreea@gmail.com>
2021-02-17 18:08:22 +0200

    Use only one class for gallery

--- resources/main.js
+++ resources/main.js
@@ -7,16 +7,13 @@ let bodyWrapper = document.getElementById('bodyWrapper');
 let mobileMenu = document.getElementById('mobileMenu');
 
 seeMenu.addEventListener('click', function(){
-    // nu ai nevoie de if in acest caz, pentru ca elementele respective sunt vizualizabile unic, nu exista ambiguitate
-    if (mobileMenu.classList.contains('hidden')) {
+        (mobileMenu.classList.contains('hidden'))
         mobileMenu.classList.remove('hidden');
         bodyWrapper.classList.add('hidden');
-    } 
 });
 
 removeMenu.addEventListener('click', function(){
-        if (!mobileMenu.classList.contains('hidden')) {
-            mobileMenu.classList.add('hidden');
-            bodyWrapper.classList.remove('hidden');
-        }
+        (!mobileMenu.classList.contains('hidden'))
+        mobileMenu.classList.add('hidden');
+        bodyWrapper.classList.remove('hidden');
 });
